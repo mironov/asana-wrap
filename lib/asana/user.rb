2093f56db35e8ab5abfc30daf432ba5462733925
Anton Mironov <ant.mironov@gmail.com>
2015-03-04 19:48:58 +0300

    one more fix

--- lib/asana/user.rb
+++ lib/asana/user.rb
@@ -3,23 +3,23 @@ module Asana
 
     def self.me(params = {})
       uri = "/users/me"
-      self.get(uri, params)
+      self.get_data(uri, params)
     end
 
     def self.get_by_id(params = {})
       uri = "/users/#{params[:user]}"
       params.delete(:user)
-      self.get(uri, params)
+      self.get_data(uri, params)
     end
 
     def self.all(params = {})
       uri = "/users"
-      self.get(uri, params)
+      self.get_data(uri, params)
     end
 
     def self.by_workspace(params ={})
       uri = "/workspaces/#{params[:workspace]}/users"
-      self.get(uri, params)
+      self.get_data(uri, params)
     end
   end
 end
